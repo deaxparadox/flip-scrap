24cb954a212c5da13efe077f4248e72bc5f81c5c
deaxparadox <nitish136900kushwaha@gmail.com>
2025-01-31 18:48:56 +0530

    integrated environment; working with selenium

--- project/sel/app.py
+++ project/sel/app.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import sys
 import re
 import os
@@ -7,7 +8,10 @@ from urllib.parse import quote_plus, unquote_plus, quote
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from bs4 import BeautifulSoup
 from time import sleep
@@ -26,43 +30,162 @@ class SearchMobile:
     phone = r"(phone|phones)"
     branch = r"(apple-iphone|apple|mi|lg|samsung|htc|honor|motorola|apple|oppo|realme|nothing)"
 
+class Chrome(ABC):
+    def __init__(
+        self,
+        options=False,
+        auto_open_devtools_for_tab=False
+    ):
+        self.options = options
+        if options:
+            if auto_open_devtools_for_tab:
+                self.chrome_add_options()
+                
+    chrome_options = webdriver.ChromeOptions()
+    
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError("Should be implemented in subclass")
+    
+    def chrome_add_options(self):
+        self.chrome_options.add_argument("--auto-open-devtools-for-tabs")
+    
+    def set_up(self):
+        if self.options:
+            self.chrome_driver = webdriver.Chrome(options=self.chrome_options)
+        else:
+            self.chrome_driver = webdriver.Chrome()
+    
+    def __call__(self):
+        return self.run()
+    
+    def teardown(self):
+        self.chrome_driver.quit()
 
-queue = Queue()
-queue.put(FlipPath.base)
 
+class Scraper(Chrome):
+    def __init__(self, quote=False):
+        super().__init__(
+            # options=True, auto_open_devtools_for_tab=True
+        )
+        
+        self.queue = Queue()
+        _url = FlipPath.base
+        if quote:
+            _url = quote_plus(_url)
+        self.queue.put(_url)
+    
+    branch = re.compile(SearchMobile.branch)
+    phone = re.compile(SearchMobile.phone)
+    
+    def hover_electronics_category(self):
+        self.chrome_driver.get(self.queue.get())
+        hovers = self.chrome_driver.find_elements(By.CLASS_NAME, "_1ch8e_")
+        for h in hovers:
+            electronics = h.get_attribute("aria-label")
+            if electronics == "Electronics":
+                webdriver.ActionChains(self.chrome_driver)\
+                    .move_to_element(h)\
+                        .perform()
+                sleep(100)
+                
+    def insert_raw(self, data, /, *, db = db.get_session()):
+        d = models.RawModel(data=data)
+        db.add(d)
+        db.commit()
+    
+    def find_blocks(self, class_id: str = None):
+        
+        try:
+            # self.chrome_driver.refresh()
+            blocks: list[WebElement] = WebDriverWait(
+                self.chrome_driver, 5
+            ).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "KzDlHZ"))
+                # EC.presence_of_element_located((By.TAG_NAME, "html"))
+            )
+            for block in blocks:
+                # img = block.find_element(By.TAG_NAME, "img")
+                # title = block.find_element(By.CLASS_NAME, "KzDlHZ")
+                # print(f"Img: {img.get_attribute("href")}\nTitle: {title.text}")
+                # print("inserting", block.text)
+                self.insert_raw(block.text)
+                # print('-'*50)
+        except Exception as e:
+            print(e)
+        
 
-branch = re.compile(SearchMobile.branch)
-phone = re.compile(SearchMobile.phone)
-
-def filter_url(url: str):
-    if branch.search(url) and phone.search(url):
-        return url
-
-def find_all_links(element: WebElement, db = db.get_session()):
-    a_tags = element.find_elements(By.TAG_NAME, 'a')
-    for a in a_tags:
-        with open("file1.txt", 'a+') as f:
-            url = a.get_attribute('href')
-            if isinstance(url, str):
-                filtered_url = filter_url(url)
-                if filtered_url:    
-                    queue.put(
-                        filtered_url   
-                    )
-            # instance = models.URLModel(url=quote_plus(filter_url(url)))
-            # db.add(instance)
-            # db.commit()
+    def parse_page(self, page: str):
+        bs = BeautifulSoup(page, "html.parser")
+        div_blocks = bs.find_all("div")
+        for div in div_blocks:
+            print(div)
             
-def get_content(driver: webdriver.Chrome) -> WebElement:
-    url = queue.get()
-    print(url, type(url))
-    driver.get(url)
-    return driver.find_element(By.TAG_NAME, 'body')
+    def search_input(self, search: str = None):
+        if not search:
+            raise RuntimeError("Search keyword cannot be None")
+        
+        # element = WebDriverWait(
+        #     self.chrome_driver, 10
+        # ).until(
+        #     EC.presence_of_element_located((By.CLASS_NAME, "Pke_EE"))
+        # )
+        element = self.chrome_driver.find_element(By.CLASS_NAME, 'Pke_EE')
+        
+        element.send_keys(search)
+        element.send_keys(Keys.ENTER)
+        self.find_blocks()
+    
+    def run(self):
+        
+        self.chrome_driver.get(
+            self.queue.get()
+        )
+        try:
+            self.search_input("Laptops")
+        except Exception as e:
+            raise RuntimeError(str(e))
+        # while not self.queue.empty():
+        #     body_element = self.get_content()
+        #     self.find_all_links(body_element)
+
+    def filter_url(self, url: str):
+        if self.branch.search(url) and self.phone.search(url):
+            return url
+        return 
+
+    def find_all_links(self, element: WebElement, db = db.get_session()):
+        a_tags = element.find_elements(By.TAG_NAME, 'a')
+        for a in a_tags:
+            with open("file1.txt", 'a+') as f:
+                url = a.get_attribute('href')
+                if isinstance(url, str):
+                    url = self.filter_url(url)
+                    if not url: continue
+                    print(url)
+                    if self._quote_url:
+                        url = quote_plus(url)
+                    if url:    
+                        self.queue.put(url)
+                        # save url in database
+                    instance = models.URLModel(url=url)
+                    db.add(instance)
+                    db.commit()
+                
+    def get_content(self) -> WebElement:
+        url = self.queue.get()
+        # print(url, type(url))
+        if self._quote_url:
+            url = unquote_plus(url) 
+        self.chrome_driver.get(url)
+        return self.chrome_driver.find_element(By.TAG_NAME, 'body')
 
      
 def main():
-    chrome_driver = webdriver.Chrome()
-    
+    scraper = Scraper()
+    scraper.set_up()
+    scraper()
+    scraper.teardown()
     # electronic_hover = chrome_driver.find_element(
     #     By.XPATH, 
     #     '//*[@id="container"]/div/div[1]/div/div/div/div/div[1]/div/div/div/div[2]/div[1]/div/div[1]/div/div/div/div/div[1]/div[2]/div/div'
@@ -70,11 +193,7 @@ def main():
     # webdriver.ActionChains(chrome_driver)\
     #     .move_to_element(electronic_hover)\
     #         .perform()
-    
-    while not queue.empty():
-        body_element = get_content(chrome_driver)
-        find_all_links(body_element)
-    chrome_driver.quit()
+
     
 if __name__ == "__main__":
     main()
--- project/sel/models.py
+++ project/sel/models.py
@@ -2,7 +2,12 @@ from sqlalchemy import Column, Integer, String, Sequence
 from db import Base
 
 class URLModel(Base):
-    __tablename__ = "users"
+    __tablename__ = "urls"
     
     id = Column(Integer, Sequence("url_id_seq"), primary_key=True)
     url = Column(String)
+    
+class RawModel(Base):
+    __tablename__ = "raw"
+    id = Column(Integer, Sequence('raw_id_seq'), primary_key=True)
+    data = Column(String)
